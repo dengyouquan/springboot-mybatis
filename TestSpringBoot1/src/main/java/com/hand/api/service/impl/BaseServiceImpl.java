490301e0d41fbca2686307e7ce2300c0a6888ee5
dengyouquan <youquan.deng@hand-china.com>
2018-08-10 12:24:44 +0800

    [ADD] city增加redis，cache，基类不能增加缓存(待解决)

--- TestSpringBoot1/src/main/java/com/hand/api/service/impl/BaseServiceImpl.java
+++ TestSpringBoot1/src/main/java/com/hand/api/service/impl/BaseServiceImpl.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageHelper;
 import com.hand.api.service.BaseService;
 import com.hand.domain.entity.BaseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.common.Mapper;
 
@@ -17,18 +19,23 @@ import java.util.List;
  **/
 @Transactional
 //泛型继承后为何要指定边界？？？
-public class BaseServiceImpl<K extends BaseEntity, T extends Mapper> implements BaseService<K> {
+//无法再这里配置Cache，因为必须有一个value,相当于zset，然后每个实现实体类key会冲突
+public abstract class BaseServiceImpl<K extends BaseEntity, T extends Mapper> implements BaseService<K> {
+
     @Autowired
     protected T mapper;
 
     @Override
-    public void save(K k) {
+    public K save(K k) throws Exception {
         mapper.insert(k);
+        return (K) mapper.selectByPrimaryKey(k);
     }
 
     @Override
-    public void update(K k) {
+    public K update(K k) {
         mapper.updateByPrimaryKey(k);
+        ////u对象中可能只有只几个有效字段，其他字段值靠数据库生成，比如id
+        return (K) mapper.selectByPrimaryKey(k);
     }
 
     @Override
@@ -36,6 +43,7 @@ public class BaseServiceImpl<K extends BaseEntity, T extends Mapper> implements 
         mapper.deleteByPrimaryKey(id);
     }
 
+    //@Cacheable(key = "#id.toString()")
     @Override
     public K getById(Short id) {
         return (K) mapper.selectByPrimaryKey(id);
